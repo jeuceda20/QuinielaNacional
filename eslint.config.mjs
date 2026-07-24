0cb366d90d2c0a7facb76ee99de19be2ca214c2a
Juan Euceda <juan.euceda@agilesolutions.com>
2026-07-24 10:56:38 -0600

    chore: configure formatting conventions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +15,31 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react$", "^react/"],
+            ["^next(?:/|$)"],
+            ["^@?\\w"],
+            ["^@/modules/"],
+            ["^@/components/"],
+            ["^@/types/"],
+            ["^@/(?:lib|hooks|services|utils)/"],
+            ["^\\."],
+            ["^\\u0000"],
+          ],
+        },
+      ],
+    },
+  },
+  prettier,
 ]);
 
 export default eslintConfig;
